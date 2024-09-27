8e4c7ebac6c25cc0ac41e6d0796182f1ce4c899d
Murillo Maiorano <79369489+maioranomu@users.noreply.github.com>
2024-09-27 19:55:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,51 +2,42 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import os
-name = "Milene Zanardi Maioranov"
+import random
+device = torch.device("cuda:0")
+if torch.cuda.is_available():
+     torch.set_default_device(device)
+
+directory = "Memory3"
+model_path = os.path.join(f"{directory}/model.pth")
+optimizer_path = os.path.join(f"{directory}/optimizer.pth")
+if not os.path.exists(directory):
+    os.makedirs(directory)
+if not os.path.exists(model_path):
+    open(model_path, 'w').close()
+if not os.path.exists(optimizer_path):
+    open(optimizer_path, 'w').close()
 """
 Region:
 1 : 6x
 2 : 3x
 3 : 1x
 """
-df = [
-    [30, 1, 180],
-    [30, 2, 90],
-    [30, 3, 30],
-    [40, 1, 240],
-    [40, 2, 120],
-    [40, 3, 40],
-    [50, 1, 300],
-    [50, 2, 150],
-    [50, 3, 50],
-    [60, 1, 360],
-    [60, 2, 180],
-    [60, 3, 60],
-    [70, 1, 420],
-    [70, 2, 210],
-    [70, 3, 70],
-    [80, 1, 480],
-    [80, 2, 240],
-    [80, 3, 80],
-    [90, 1, 540],
-    [90, 2, 270],
-    [90, 3, 90],
-    [100, 1, 600],
-    [100, 2, 300],
-    [100, 3, 100],
-    [110, 1, 660],
-    [110, 2, 330],
-    [110, 3, 110],
-    [120, 1, 720],
-    [120, 2, 360],
-    [120, 3, 120],
-    [130, 1, 780],
-    [130, 2, 390],
-    [130, 3, 130],
-    [140, 1, 840],
-    [140, 2, 420],
-    [140, 3, 140],
-]
+
+examples = 250
+df = []
+for i in range(examples):
+    price = 0
+    m2 = random.randint(1, 501)
+    region = random.randint(1, 4)
+    if region == 1:
+        price = m2 * 6
+    elif region == 2:
+        price = m2 * 3
+    else:
+        price = m2
+    df.append([m2, region, price])
+# print(df)
+print("\n")
 
 x = torch.tensor([[i[0], i[1]] for i in df], dtype=torch.float32)  # Input: m² and region
 y = torch.tensor([[i[2]] for i in df], dtype=torch.float32)          # Target: price
@@ -60,8 +51,6 @@ model = nn.Sequential(
 )
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-model_path = os.path.join("Memory/model.pth")
-optimizer_path = os.path.join("Memory/optimizer.pth")
 try:
     model.load_state_dict(torch.load(model_path))
     optimizer.load_state_dict(torch.load(optimizer_path))
@@ -74,22 +63,34 @@ except:
         loss.backward()                      # Set Backward
         optimizer.step()                     # Update weights
 
-        # print(f"Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}")
+        if epoch == 40000:
+            optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+        print(f"Epoch [{epoch + 1}/{epochs}] | Loss: [{loss.item():.4f}]")
         if (epoch + 1) % 500 == 0:
-            print(f"Epoch [{epoch + 1}/{epochs}] | Loss: {loss.item():.4f}")
+            print(f"Epoch [{epoch + 1}/{epochs}] | Loss: [{loss.item():.4f}]")
             if loss.item() <= 0.0001:
                 break
     predictions = model(x)
     final_loss = loss_fn(predictions, y)
     print(f"\nFinal Loss after training: {final_loss.item():.4f}")
 
-    torch.save(model.state_dict(), model_path)  # Salva o estado do modelo
-    torch.save(optimizer.state_dict(), optimizer_path)  # Salva o estado do otimizador
+    torch.save(model.state_dict(), model_path)
+    torch.save(optimizer.state_dict(), optimizer_path)
     print("Saved")
 
 #------------------------------------------
-tx = torch.tensor([[100, 1]], dtype=torch.float32)
-ty = torch.tensor([[600]], dtype=torch.float32)
+tm2 = 177
+tr = 1
+t_price = 0
+if tr == 1:
+    t_price = tm2 * 6
+elif region == 2:
+    tr = tm2 * 3
+else:
+    t_price = m2
+tx = torch.tensor([[tm2, tr]], dtype=torch.float32)
+ty = torch.tensor([[t_price]], dtype=torch.float32)
 
 predicted_price = model(tx)
 
